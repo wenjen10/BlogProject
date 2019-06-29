40bd9f3386f9e5d92c58b29d4f1ff0e1359c6e4d
CLux <cplux7@gmail.com>
2019-06-29 12:22:06 -0400

    started edits to startup class

--- JCBlogProject/Repositories/IRepository.cs
+++ JCBlogProject/Repositories/IRepository.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace JCBlogProject.Repositories
 {
-    public class IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        //void Create(T obj);
+        //void Ddelete(T obj);
+        //void Edit(T obj); 
     }
 }
--- JCBlogProject/Startup.cs
+++ JCBlogProject/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using JCBlogProject.Models;
+using JCBlogProject.Repositories;
 
 namespace JCBlogProject
 {
@@ -14,8 +16,12 @@ namespace JCBlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {//testit
-
+        {
+            services.AddMvc();
+            services.AddDbContext<BlogContext>();
+            services.AddScoped<IRepository<Post>, PostRepository>();
+            services.AddScoped<IRepository<Genre>, GenreRepository>();
+            services.AddScoped<IRepository<Tag>, TagRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
